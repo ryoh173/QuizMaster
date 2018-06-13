663babdb28bcf89f27ebbb9a059c6babec8f31eb
hasebe <hasebe@appirits.com>
2018-06-13 15:12:29 +0900

    seedデータ追加

--- README.md
+++ README.md
@@ -15,4 +15,5 @@
 - `bundle install --path vendor/bundle`
 - `bundle exec rails db:create`
 - `bundle exec rails db:migrate`
+- `bundle exec rails db:seed`
 - `bundle exec rails s`
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# coding: utf-8
+
+Question.create(content: 'アルファベットの数は？', answer: '26')
+Question.create(content: '山手線の駅の数は？', answer: '29')
+Question.create(content: '人間の骨の数は？', answer: '206')
+Question.create(content: '世界の国の数は？', answer: '196')
+Question.create(content: 'アメリカの州の数は？', answer: '50')
+Question.create(content: 'ハリセンボンの針の数は？', answer: '712')
+Question.create(content: '国民の祝日は年間何日？', answer: '15')
+Question.create(content: '東京都に村はいくつある？', answer: '8')
+Question.create(content: '東京都にある大学の数は？', answer: '137')
+Question.create(content: '今年は何年？', answer: '2018')
